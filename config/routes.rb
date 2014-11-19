cd353dad9917972969c4e45a8bd32793346398cd
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-11-19 18:27:02 +0800

    add sponsors to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :students
   resources :companies
+  resources :sponsors
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

a8f21ae7a6748ba75b4b1d6ffe6dcc3654d5ea2a
befong000 <befong000@gmail.com>
2015-01-06 02:02:44 +0800

    Add Data base-sponsors:name,description,mission,prize

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -1,12 +1,21 @@
 class SponsorsController < ApplicationController
 
   def index
+      @sponsors = Sponsor.all 
   end
-
   def show
+      if !current_user
+        redirect_to user_session_path
+      end
+
+      @sponsor = Sponsor.find(params[:id])
   end
 
   def new
+  end 
+  
+  def each
+
   end 
 
   def create

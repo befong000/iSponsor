91b38381a7a3d8877236e86bfb87c3058dfe9735
chihsuan, haung <chihsuan.huang.7@gmail.com>
2015-01-10 18:58:41 +0800

    update

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -1,12 +1,24 @@
 class SponsorsController < ApplicationController
 
   def index
+      @sponsors = Sponsor.all 
   end
-
+  
   def show
+
+     @sponsors = Sponsor.find_by_id(params[:id])
+         if !current_user
+        redirect_to user_session_path
+        end   
+      
+      
   end
 
   def new
+  end 
+  
+  def each
+
   end 
 
   def create

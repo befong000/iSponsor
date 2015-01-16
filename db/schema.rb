9305b20f280a07bad45d4671c795b45cf387aa2b
Amo Chen <t9525838@gmail.com>
2015-01-17 01:37:03 +0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,29 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150116150102) do
+ActiveRecord::Schema.define(version: 20150104145600) do
 
   create_table "sponsors", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.text     "mission"
-    t.string   "prize"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "tasks", force: true do |t|
-    t.integer  "phone"
-    t.string   "club"
-    t.integer  "club_number"
-    t.string   "university"
-    t.boolean  "sendemailornot"
-    t.integer  "mailnumber"
-    t.string   "fanpagelink"
-    t.integer  "fanpagenumber"
-    t.boolean  "shareinfoornot"
-    t.date     "finishsponsordate"
-    t.integer  "user_id"
+    t.string   "reward"
+    t.text     "img_URL_outer"
+    t.integer  "img_URL_inner"
+    t.text     "event_time"
+    t.text     "notice"
+    t.text     "Participants"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
